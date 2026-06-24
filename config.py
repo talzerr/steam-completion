914b4eb4613b8a9e48506647c3aec007c97e2133
talzerr <tal.koviazin@connecteam.com>
2026-06-24 23:29:42 +0300

    chore: bootstrap project dependencies and config

---
+++ config.py
@@ -0,0 +1,21 @@
+STEAM_API_KEY = "your_api_key_here"
+STEAM_ID = "your_steam_id_here"
+
+SMALL_RETURN = 4
+SMALL_POOL = 15
+BIG_RETURN = 2
+BIG_POOL = 10
+STALENESS_THRESHOLD_YEARS = 2
+FRESHNESS_RECENT_MONTHS = 6
+
+SCORING_WEIGHTS = {
+    "ease": 0.35,
+    "size": 0.25,
+    "difficulty": 0.25,
+    "freshness": 0.15,
+}
+
+MULTIPLAYER_KEYWORDS = [
+    "co-op", "coop", "multiplayer", "versus", "pvp",
+    "with a friend", "online", "competitive",
+]
